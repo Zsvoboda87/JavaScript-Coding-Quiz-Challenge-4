075b7d8bc7d8258465f8c8a0cb6c39327bac2a9c
Zsvob <svobodazachary@gmail.com>
2021-12-09 15:31:33 -0500

    timer logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,46 @@
 var timer = 60;
+var countdown = function(){
+    console.log(timer);
+    timer--; 
+    if (timer === -1) {
+        clearInterval(startCountdown)
+    };
+};
+
+
 var startButton = document.querySelector("#start-button");
 var questionDisplayEl = document.querySelector("#question-display");
+var displayScore = document.querySelector("#HighScore");
 var score = 0;
+
 
 var createQuestion1 = function() {
     questionDisplayEl.removeChild(startButton);
+    setInterval(countdown, 1000);
 
     var question1El = document.createElement("div");
     question1El.className = "questions";
-    question1El.textContent = "Who is your favorite Mario character?";
+    question1El.textContent = "What does HTML stand for?";
     questionDisplayEl.appendChild(question1El);
     
     var q1a1El = document.createElement("button");
     q1a1El.className = "questions-button";
-    q1a1El.textContent = "Princess Peach";
+    q1a1El.textContent = "HyperText Markup Language";
     questionDisplayEl.appendChild(q1a1El);
     
     var q1a2El = document.createElement("button");
     q1a2El.className = "questions-button";
-    q1a2El.textContent = "Super Mario";
+    q1a2El.textContent = "HyperText Mechanical Language";
     questionDisplayEl.appendChild(q1a2El);
 
     var q1a3El = document.createElement("button");
     q1a3El.className = "questions-button";
-    q1a3El.textContent = "Luigi";
+    q1a3El.textContent = "HyperText Monkey Language";
     questionDisplayEl.appendChild(q1a3El);
 
     var q1a4El = document.createElement("button");
     q1a4El.className = "questions-button";
-    q1a4El.textContent = "Wario";
+    q1a4El.textContent = "HeroText Mechanical Language";
     questionDisplayEl.appendChild(q1a4El);
 
     var nextQuestionCorrect = function () {
@@ -38,7 +50,9 @@ var createQuestion1 = function() {
         questionDisplayEl.removeChild(q1a3El);
         questionDisplayEl.removeChild(q1a4El);
         console.log("right");
-        createQuestion2();    
+        score = score +10;
+        createQuestion2();
+        displayCurrentScore();    
     };
     
     var nextQuestionIncorrect = function() {
@@ -48,6 +62,7 @@ var createQuestion1 = function() {
         questionDisplayEl.removeChild(q1a3El);
         questionDisplayEl.removeChild(q1a4El);
         console.log("wrong")
+        timer = timer -10;
         createQuestion2();
     };
 
@@ -57,34 +72,130 @@ var createQuestion1 = function() {
     q1a4El.addEventListener("click", nextQuestionIncorrect);
 };
 
-
 var createQuestion2 = function() {
+    
     var question2El = document.createElement("div");
     question2El.className = "questions";
-    question2El.textContent = "Who is your Ninja Turtle?";
+    question2El.textContent = "Favorite color";
     questionDisplayEl.appendChild(question2El);
     
     var q2a1El = document.createElement("button");
     q2a1El.className = "questions-button";
-    q2a1El.textContent = "red";
+    q2a1El.textContent = "Red";
     questionDisplayEl.appendChild(q2a1El);
     
     var q2a2El = document.createElement("button");
     q2a2El.className = "questions-button";
-    q2a2El.textContent = "orange";
+    q2a2El.textContent = "Blue";
     questionDisplayEl.appendChild(q2a2El);
 
     var q2a3El = document.createElement("button");
     q2a3El.className = "questions-button";
-    q2a3El.textContent = "purple";
+    q2a3El.textContent = "Green";
     questionDisplayEl.appendChild(q2a3El);
 
     var q2a4El = document.createElement("button");
     q2a4El.className = "questions-button";
-    q2a4El.textContent = "blue";
+    q2a4El.textContent = "Purple";
     questionDisplayEl.appendChild(q2a4El);
 
+    var nextQuestionCorrect = function () {
+        questionDisplayEl.removeChild(question2El);
+        questionDisplayEl.removeChild(q2a1El);
+        questionDisplayEl.removeChild(q2a2El);
+        questionDisplayEl.removeChild(q2a3El);
+        questionDisplayEl.removeChild(q2a4El);
+        console.log("right");
+        score = score +10;
+        createQuestion3();    
+    };
+    
+    var nextQuestionIncorrect = function() {
+        questionDisplayEl.removeChild(question2El);
+        questionDisplayEl.removeChild(q2a1El);
+        questionDisplayEl.removeChild(q2a2El);
+        questionDisplayEl.removeChild(q2a3El);
+        questionDisplayEl.removeChild(q2a4El);
+        console.log("wrong")
+        timer = timer -10;
+        createQuestion3();
+    };
+
+    q2a1El.addEventListener("click", nextQuestionCorrect);
+    q2a2El.addEventListener("click", nextQuestionIncorrect);
+    q2a3El.addEventListener("click", nextQuestionIncorrect);
+    q2a4El.addEventListener("click", nextQuestionIncorrect);
 };
+
+var createQuestion3 = function() {
+
+    var question3El = document.createElement("div");
+    question3El.className = "questions";
+    question3El.textContent = "Who is your favorite Mario character?";
+    questionDisplayEl.appendChild(question3El);
+    
+    var q3a1El = document.createElement("button");
+    q3a1El.className = "questions-button";
+    q3a1El.textContent = "Princess Peach";
+    questionDisplayEl.appendChild(q3a1El);
+    
+    var q3a2El = document.createElement("button");
+    q3a2El.className = "questions-button";
+    q3a2El.textContent = "Super Mario";
+    questionDisplayEl.appendChild(q3a2El);
+
+    var q3a3El = document.createElement("button");
+    q3a3El.className = "questions-button";
+    q3a3El.textContent = "Luigi";
+    questionDisplayEl.appendChild(q3a3El);
+
+    var q3a4El = document.createElement("button");
+    q3a4El.className = "questions-button";
+    q3a4El.textContent = "Wario";
+    questionDisplayEl.appendChild(q3a4El);
+
+    var nextQuestionCorrect = function () {
+        questionDisplayEl.removeChild(question3El);
+        questionDisplayEl.removeChild(q3a1El);
+        questionDisplayEl.removeChild(q3a2El);
+        questionDisplayEl.removeChild(q3a3El);
+        questionDisplayEl.removeChild(q3a4El);
+        console.log("right");
+        score = score +10;
+        //quizerName();
+        createHighScore();   
+    };
+    
+    var nextQuestionIncorrect = function() {
+        questionDisplayEl.removeChild(question3El);
+        questionDisplayEl.removeChild(q3a1El);
+        questionDisplayEl.removeChild(q3a2El);
+        questionDisplayEl.removeChild(q3a3El);
+        questionDisplayEl.removeChild(q3a4El);
+        console.log("wrong")
+        timer = timer -10;
+        //quizerName();
+        createHighScore();
+    };
+
+    q3a1El.addEventListener("click", nextQuestionCorrect);
+    q3a2El.addEventListener("click", nextQuestionIncorrect);
+    q3a3El.addEventListener("click", nextQuestionIncorrect);
+    q3a4El.addEventListener("click", nextQuestionIncorrect);
+};
+
+var createQuestion4 = function() {
+    console.log(timer);
+    console.log(score);
+}
+
+var quizerName = function() {
+    value = window.prompt("What's Your Name")
+    return value;
+}
+var createHighScore = function() {
+    window.alert(quizerName() + " has a High Score of " + score)
+} 
 
 
 
